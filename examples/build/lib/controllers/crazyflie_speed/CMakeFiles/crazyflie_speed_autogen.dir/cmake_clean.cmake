5c89d8c9dd95ad96dd1237d1c85973e9a7a34bb9
Marck Herzon Barrion <barrion.herzon@gmail.com>
2024-04-21 01:45:51 +0800

    Delete examples/build directory

--- examples/build/lib/controllers/crazyflie_speed/CMakeFiles/crazyflie_speed_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/crazyflie_speed_autogen"
-  "crazyflie_speed_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/crazyflie_speed_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
